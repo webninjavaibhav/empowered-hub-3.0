30bb7872e080b3863f6f94b0b4e29bb3269dc13f
vaibhavpatel <dev.vaibhav.patel@gmail.com>
2024-07-30 17:56:59 +0530

    change in env

--- src/helpers/Okta.tsx
+++ src/helpers/Okta.tsx
@@ -1,5 +1,5 @@
 const BASENAME = import.meta.env.BASE_URL || "";
-const REDIRECT_URI = import.meta.env.VITE_REDIRECT_URL;
+const REDIRECT_URI = "https://empowered-hub-3-0.vercel.app/login/callback";
 
 export default {
   oidc: {
